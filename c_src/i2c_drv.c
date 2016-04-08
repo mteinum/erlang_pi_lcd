fdff2ee008b955b96030d70ace403fe02239d9e4
Morten Teinum <morten.teinum@gmail.com>
2016-04-08 23:02:50 +0000

    splitted control metod

--- c_src/i2c_drv.c
+++ c_src/i2c_drv.c
@@ -16,6 +16,7 @@
 #include "erl_driver.h"
 
 // commands for (*control)
+
 #define CMD_WRITE_BLOCK_DATA 3
 #define CMD_OPEN             4
 
@@ -49,51 +50,58 @@ static void i2c_drv_stop(ErlDrvData handle)
     driver_free((char*)handle);
 }
 
-static ErlDrvSSizeT i2c_drv_ctl(ErlDrvData handle, 
-                 unsigned int cmd, char* buf0, ErlDrvSizeT len,
-                 char** rbuf, ErlDrvSizeT rsize)
-{
-    __s32 result, ret;
+static void i2c_drv_ctl_write_block_data(i2c_drv_portdata* d, char* buf0, ErlDrvSizeT len) {
+    
+    __s32 result;
     union i2c_smbus_data data;
-    char driver_path[0x1000];
-    int addr = 0x20;
 
-    i2c_drv_portdata* d = (i2c_drv_portdata*)handle;
+    data.block[0] = 2;
+    data.block[1] = buf0[1];
+    data.block[2] = buf0[2];
 
-    if (cmd == CMD_WRITE_BLOCK_DATA && len == 3) {
-
-        data.block[0] = 2;
-        data.block[1] = buf0[1];
-        data.block[2] = buf0[2];
-
-        result = i2c_smbus_access(
+    result = i2c_smbus_access(
             d->fd,
             I2C_SMBUS_WRITE,
             (__u8)buf0[0],
             I2C_SMBUS_I2C_BLOCK_BROKEN,
             &data);
 
-        if (result || d->verbose)
+    if (result || d->verbose)
             fprintf(stderr, "i2c_smbus_write_block_data reg=0x%02X: [%i, %i] result=%i\r\n",
                 buf0[0], buf0[1], buf0[2], result);
+}
 
-        return 0;
-    }
-    else if (cmd == CMD_OPEN) {
-        memset(driver_path, 0, sizeof(driver_path));
-        memcpy(driver_path, buf0, len);
+static void i2c_drv_ctl_open(i2c_drv_portdata* d, char* buf0, ErlDrvSizeT len){
+    int ret;
+    char driver_path[0x1000];
+    int addr = 0x20;
+    memset(driver_path, 0, sizeof(driver_path));
+    memcpy(driver_path, buf0, len);
 
-        d->fd = open(driver_path, O_RDWR, 0);
+    d->fd = open(driver_path, O_RDWR, 0);
+
+    if (d->verbose)
+        fprintf(stderr, "open %i\r\n", d->fd);
+    
+    if (d->fd) {
+        ret = ioctl(d->fd, I2C_SLAVE, addr);
 
         if (d->verbose)
-            fprintf(stderr, "open %i\r\n", d->fd);
-    
-        if (d->fd) {
-            ret = ioctl(d->fd, I2C_SLAVE, addr);
+            fprintf(stderr, "ioctl %i\r\n", ret);
+    }
+}
 
-            if (d->verbose)
-                fprintf(stderr, "ioctl %i\r\n", ret);
-        }
+static ErlDrvSSizeT i2c_drv_ctl(ErlDrvData handle, 
+                 unsigned int cmd, char* buf0, ErlDrvSizeT len,
+                 char** rbuf, ErlDrvSizeT rsize)
+{
+    i2c_drv_portdata* d = (i2c_drv_portdata*)handle;
+
+    if (cmd == CMD_WRITE_BLOCK_DATA && len == 3) {
+        i2c_drv_ctl_write_block_data(d, buf0, len);
+    }
+    else if (cmd == CMD_OPEN) {
+        i2c_drv_ctl_open(d, buf0, len);
     }
 
     return 0;
